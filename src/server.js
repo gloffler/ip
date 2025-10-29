57124583df159c7fbc6bcd2469981b979525155d
Gregor <174007895+gloffler@users.noreply.github.com>
2025-10-29 20:18:40 +0100

    fix: remove IPv6 prefix on mapped IP addresses

--- src/server.js
+++ src/server.js
@@ -4,10 +4,11 @@ const app = express();
 app.set("trust proxy", true);
 
 app.get("/", (req, res) => {
-  const ip =
+  let ip =
     (req.headers["x-forwarded-for"] || "").split(",").shift().trim() ||
     req.socket.remoteAddress;
-  res.json({ ip });
+  if (ip.startsWith("::ffff:")) ip = ip.substring(7);
+  res.json(ip);
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
